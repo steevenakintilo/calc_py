c5d973756976e78dbcfe6c031d8e7c3c40435ad2
steevenakintilo <72024403+steevenakintilo@users.noreply.github.com>
2022-03-20 13:20:56 +0100

    okok

--- main.py
+++ main.py
@@ -35,6 +35,33 @@ def check_file():
 
 
 def write_num(x):
+    num_one_l = num_one_list()
+    num_two_l = num_two_list()
+    all_inp = print_file("inp")
+    op_list = print_file("all_op")
+    op_l = make_list_from_op(op_list)
+    #print(all_inp,num_one_l)
+    #print(int(all_inp)+1)
+    operant = "+*-/"
+    if len(op_l) > 0:
+        last_op = op_l[len(op_l) - 1]
+    else:
+        last_op = 0
+    if len(op_l) > 0:
+        before_last_op = op_l[len(op_l) - 2]
+    else:
+        before_last_op = 0
+    
+    if len(op_l) > 0:
+        if int(all_inp) in num_one_l:
+            #print("Num 1 " + str(all_inp))
+            wwrite_into_file("num1",x)
+            
+        elif int(all_inp) in num_two_l:
+            #print("Num 2 " + str(all_inp))
+            wwrite_into_file("window",x)
+        #else:
+            #print("Operant " + str(all_inp))
     write_into_file("check","num")
     number_one = print_file("num1")
     number_two = print_file("num2")
@@ -44,14 +71,21 @@ def write_num(x):
         write_into_file("num1","")
         write_into_file("num2","")
     if x != -1:
-        if check == "op" or check == "num":
-            if number_one != "" and number_two != "":
+        if number_one != "" and number_two != "":
+            #print("aaaaaaaaaaaa")
+            if before_last_op in operant:
+                print("ccacc")
                 write_into_file("num1",res)
                 write_into_file("num2",x)
-            elif number_one == "" and number_two == "":
-                write_into_file("num1",x)
-            elif number_one != "" and number_two == "":
-                write_into_file("num2",x)    
+            else:
+                print("bBBBba")
+                wwrite_into_file("num2",x)
+        elif number_one == "" and number_two == "" and int(all_inp) not in num_two_l:
+            print("bbbbbbbbbbbb")
+            write_into_file("num1",x)
+        elif number_one != "" and number_two == "" and int(all_inp) not in num_one_l:
+            print("cccccccccccc")
+            wwrite_into_file("num2",x)    
 def calc_res(number_one,op,number_two):
     res = 0
     r = print_file("res")
@@ -104,13 +138,13 @@ def calc_res(number_one,op,number_two):
             except:
                 res = 0
         else:
-            res = int(number_one) / int(number_two)
+            res = int(int(number_one) / int(number_two))
             write_into_file("res",str(int(res)))
             write_into_file("num1",str(res))
             write_into_file("num2","")
     if op == "":
         write_into_file("res",r)
-    print(res,number_one,number_two)
+    #print(res,number_one,number_two)
 
 
 def make_list_from_op(op_l):
@@ -118,7 +152,25 @@ def make_list_from_op(op_l):
     for i in range(len(op_l)):
         op_list.append(op_l[i])
     return (op_list)
+
+def num_one_list():
+    l = []
+    idx = 1
+    for i in range(1,1000):
+        l.append(idx)
+        idx = idx + 4
+    return (l)
+    
+def num_two_list():
+    l = []
+    idx = 2
+    for i in range(1,1000):
+        l.append(idx)
+        idx = idx + 4
+    return (l)
 def which_button(b,display_res):
+    list_one = num_one_list()
+    list_two = num_two_list()
     button_pressed = print_file("bpress")
     number_of_button_pressed = int(button_pressed)
     number_one = print_file("num1")
@@ -137,6 +189,7 @@ def which_button(b,display_res):
     else:
         calc_res(number_one,op,number_two)    
     if b == "0":
+        write_into_file("window","300x300")
         write_num("7")
         wwrite_into_file("all_op","7")
     elif b == "1":
@@ -163,7 +216,7 @@ def which_button(b,display_res):
         wwrite_into_file("all_op","6")
     elif b == "7":
         write_into_file("op","*")
-        wwrite_into_file("all_op","7")
+        wwrite_into_file("all_op","*")
         number_of_button_pressed = number_of_button_pressed + 1
         write_into_file("bpress",str(number_of_button_pressed))
         write_into_file("check","op")
@@ -190,8 +243,9 @@ def which_button(b,display_res):
     elif b == "13":
         write_into_file("num1","")
         write_into_file("num2","")
-        wwrite_into_file("all_op","")
+        write_into_file("all_op","")
         write_into_file("bpress","0")
+        write_into_file("inp","1")
     elif b == "14":
         print("=")
         number_of_button_pressed = number_of_button_pressed + 1
@@ -204,19 +258,37 @@ def which_button(b,display_res):
         write_into_file("bpress",str(number_of_button_pressed))
         write_into_file("check","num")
         wwrite_into_file("all_op","+")
+    op_list = print_file("all_op")
+    op_l = make_list_from_op(op_list)
+    all_input = print_file("inp")
+    int_input = int(all_input)
+    if len(op_l) > 1 and (op_l[len(op_l) - 1] in digit and op_l[len(op_l) - 2] in operant):
+        int_input = int_input + 1
+        write_into_file("inp",str(int_input))
+    if len(op_l) > 1 and (op_l[len(op_l) - 1] in operant and op_l[len(op_l) - 2] in digit):
+        int_input = int_input + 1
+        write_into_file("inp",str(int_input))
+    if len(op_l) > 0:
+        last_op = op_l[len(op_l) - 1]
+    else:
+        last_op = 0
     if number_of_button_pressed == 0:
         r = print_file("num1")
-        write_into_file("res","0")
+        write_into_file("res",r)
     elif number_of_button_pressed == 1:
         r = print_file("num2")    
     else:
-        r = print_file("res")
+        if str(last_op) in digit:
+            r = last_op
+        else:
+            r = print_file("res")
     display_res["text"] = r
 
 def size_1():
    text.config(font=('Helvatical bold',20))
 
 l = []
+write_into_file("inp","1")
 write_into_file("all_op","")
 write_into_file("check","num")
 write_into_file("num1","")
@@ -252,5 +324,6 @@ for i in range(len(button_list)):
                 command=lambda m=str(i): which_button(m,display_res))
 
     button_list[i].place(x=px[i], y=py[i])
+a = print_file("window")
 app.geometry("200x250")
 app.mainloop()
